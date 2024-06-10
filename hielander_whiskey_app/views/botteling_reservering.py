e3e4e705f6aa201d1d7d27cc0fce0a358b56744b
siepvan020 <124787533+siepvan020@users.noreply.github.com>
2024-06-10 13:51:23 +0200

    Botteling form handling begin HTML update om te matchen met model fields Totaalprijs verwijderd uit modelform, deze wordt later handmatig berekend. Korte form handling en dummy save

--- hielander_whiskey_app/views/botteling_reservering.py
+++ hielander_whiskey_app/views/botteling_reservering.py
@@ -4,7 +4,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Local imports
+from hielander_whiskey_app.models import BottelingReserveringen
+from hielander_whiskey_app.forms import BottelingReserveringenForm
 
 
 def botteling_reservering_page(request: WSGIRequest) -> HttpResponse:
+    if request.method == 'POST':
+
+        # Form valideren
+        form = BottelingReserveringenForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=False)
+            print(form.cleaned_data)
+            print(form.errors)
+
+
     return render(request, 'botteling_reservering.html')
