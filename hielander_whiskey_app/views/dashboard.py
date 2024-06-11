52543016a09850434c0445ce14edfc09fcb740c7
Lucas de Vries <s1134748@student.hsleiden.nl>
2024-06-11 16:55:51 +0200

    Begin aan de back-end van de admin dashboard pagina.

--- hielander_whiskey_app/views/dashboard.py
+++ hielander_whiskey_app/views/dashboard.py
@@ -2,9 +2,43 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.shortcuts import render
+from bokeh.plotting import figure, output_file, show
+from django.db.models import Count
+from django.contrib.auth.decorators import login_required
+
+from hielander_whiskey_app.models import BottelingReserveringen
+from hielander_whiskey_app.models import MasterclassReserveringen
 
 # Local imports
 
-
+@login_required
 def dashboard_page(request: WSGIRequest) -> HttpResponse:
+    # bottel_piechart()
+    # masterclass_barplot()
+
+    Aant_botteling = BottelingReserveringen.objects.all().count
+    # counts = MasterclassReserveringen.objects.values('sessie_nummer', 'masterclass').annotate(count=Count('id????'))
+    # print(counts)
+
+
+    Botteling = BottelingReserveringen.objects.all()
+    Masterclass = MasterclassReserveringen.objects.all()
     return render(request, 'dashboard.html')
+
+
+def bottel_piechart():
+    output_file("templates/bottel_piechart.html")
+    graph = figure(title="Bottel Piechart")
+
+
+    graph.wedge()
+    pass
+
+
+def masterclass_barplot():
+    output_file("templates/masterclass_barplot.html")
+    graph = figure(title="Masterclass Barplot")
+
+
+    graph.vbar()
+    pass
