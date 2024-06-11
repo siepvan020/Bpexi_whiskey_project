7ce98d0ec524497807ac8bb8ec273f57f5112f42
siepvan020 <124787533+siepvan020@users.noreply.github.com>
2024-06-11 10:47:22 +0200

    Reservelijst-boolean update Field Reserve wordt True als er geen flessen meer over zijn

--- hielander_whiskey_app/views/botteling_reservering.py
+++ hielander_whiskey_app/views/botteling_reservering.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib import messages
+from django.db.models import Sum
 
 # Local imports
 from hielander_whiskey_app.models import BottelingReserveringen
@@ -11,6 +12,11 @@ from hielander_whiskey_app.forms import BottelingReserveringenForm
 
 
 def botteling_reservering_page(request: WSGIRequest) -> HttpResponse:
+
+    totaal_flessen = BottelingReserveringen.objects.aggregate(
+                totaal_flessen=Sum('aantal_flessen'))['totaal_flessen']
+
+
     if request.method == 'POST':
 
         # Form valideren
@@ -18,7 +24,16 @@ def botteling_reservering_page(request: WSGIRequest) -> HttpResponse:
 
         if form.is_valid():
             reservering = form.save(commit=False)
+            # Berekening van aantal flessen * prijs per fles
             reservering.totaalprijs = reservering.aantal_flessen * 50
+            
+            if totaal_flessen:
+                # Aantal flessen over na de reservering
+                na_reserv = 150 - totaal_flessen - reservering.aantal_flessen
+
+                if na_reserv < 0:
+                    reservering.reserve = True
+
             reservering.save()
             print(f'Reservering "{reservering}" opgeslagen')
 
@@ -30,7 +45,5 @@ def botteling_reservering_page(request: WSGIRequest) -> HttpResponse:
                 messages.error(request, 'E-mailadres is niet correct')
             else:
                 messages.error(request, 'Reservering niet correct')
-
-
 
     return render(request, 'botteling_reservering.html')
