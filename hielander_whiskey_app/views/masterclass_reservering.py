083b1bbbda6f8ff03fa0b13ff9c577f53f356749
BrianVonk <s1105016@student.hsleiden.nl>
2024-06-12 09:19:48 +0200

    Masterclass reservering back-end basics begin gemaakt

--- hielander_whiskey_app/views/masterclass_reservering.py
+++ hielander_whiskey_app/views/masterclass_reservering.py
@@ -1,10 +1,29 @@
 # Third party imports
 from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
+from django.contrib import messages
 
 # Local imports
+from hielander_whiskey_app.models import MasterclassReserveringen
+from hielander_whiskey_app.forms import MasterclassReserveringenForm
 
 
 def masterclass_reservering_page(request: WSGIRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = MasterclassReserveringenForm(request.POST)
+        if form.is_valid():
+            reservering = form.save(commit=False)
+            reservering.save()
+            print(f'Reservering "{reservering}" opgeslagen')
+
+            return HttpResponseRedirect(reverse('masterclass_bevestiging'))
+        else:
+            print('Reservering niet correct/form ongeldig')
+            if 'e_mailadres' in form.errors:
+                messages.error(request, 'E-mailadres is niet correct')
+            else:
+                messages.error(request, 'Reservering niet correct')
+
     return render(request, 'masterclass_reservering.html')
