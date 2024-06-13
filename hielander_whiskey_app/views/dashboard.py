2d53989323a9ceaabca3cecb15baff713c374742
Lucas de Vries <s1134748@student.hsleiden.nl>
2024-06-13 14:04:50 +0200

    Aantal gereserveerde flessen tegenover het maximaal aantal beschikbare flessen uit de FestivalData. En kleine aanpassingen aan algehele structuur.

--- hielander_whiskey_app/views/dashboard.py
+++ hielander_whiskey_app/views/dashboard.py
@@ -3,40 +3,46 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.shortcuts import render
 from bokeh.plotting import figure, output_file, show
-from django.db.models import Count
+from django.db.models import Count, Sum
 from django.contrib.auth.decorators import login_required
 
 from hielander_whiskey_app.models import BottelingReserveringen
 from hielander_whiskey_app.models import MasterclassReserveringen
+from hielander_whiskey_app.models import FestivalData
 
 # Local imports
 
-@login_required
-def dashboard_page(request: WSGIRequest) -> HttpResponse:
-    Aant_botteling = BottelingReserveringen.objects.all().count
-    # counts = MasterclassReserveringen.objects.values('sessie_nummer', 'masterclass').annotate(count=Count('id????'))
-    # print(counts)
 
-    toont1 = True
+# @login_required
+def dashboard_page(request: WSGIRequest) -> HttpResponse:
+    aantallen = []
+    totaal_flessen = BottelingReserveringen.objects.aggregate(totaal_flessen=Sum('aantal_flessen'))['totaal_flessen']
+    max_flessen = FestivalData.objects.get(naam="Ardmore (Ardlair) 2014 57.5%").aantal_beschikbaar # deze naam staat er momenteel hardcoded in omdat er geen andere manier is om dit te koppelen. Dit is een probleem in onze "erd". Dit geld voor flessen als masterclasses.
+    fles_naam = FestivalData.objects.get(naam="Ardmore (Ardlair) 2014 57.5%").naam # zelfde probleem als hierboven. Deze line is nu namelijk wel erg overbodig.
+
+    fles_line = f"{totaal_flessen}/{max_flessen} van de {fles_naam} gereserveerd"
+    aantallen.append(fles_line)
+
+    # counts = MasterclassReserveringen.objects.values('sessie_nummer', 'masterclass').annotate(count=Count('id'))
+
+
     # bottel_piechart()
     # masterclass_barplot()
 
 
 
-    toont2 = True
+
     botteling = BottelingReserveringen.objects.all()
     masterclass = MasterclassReserveringen.objects.all()
     return render(request, 'dashboard.html', {
-        'aantallen': Aant_botteling,
-        'toont1': toont1,
-        'toont2': toont2,
+        'aantallen': aantallen,
         'botteling': botteling,
         'masterclass': masterclass,
     })
 
 
 def bottel_piechart():
-    output_file("templates/bottel_piechart.html")
+    output_file("templates/grafieken/bottel_piechart.html")
     graph = figure(title="Bottel Piechart")
 
 
@@ -45,7 +51,7 @@ def bottel_piechart():
 
 
 def masterclass_barplot():
-    output_file("templates/masterclass_barplot.html")
+    output_file("templates/grafieken/masterclass_barplot.html")
     graph = figure(title="Masterclass Barplot")
 
 
