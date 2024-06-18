bf90556d8bd53023e70ff968a895e3a800552de0
BrianVonk <s1105016@student.hsleiden.nl>
2024-06-18 08:42:21 +0200

    Masterclasses veranderen na sessie selectie

--- hielander_whiskey_app/static/js/masterclass_reservering.js
+++ hielander_whiskey_app/static/js/masterclass_reservering.js
@@ -1,1 +1,30 @@
-console.log('Script geladen!!')
+console.log('Script geladen!!');
+
+function sessie_selectie() {
+    waarde = document.getElementById('sessie_nummer').value;
+    if (waarde == '1') {
+        console.log('sessie 1');
+        document.getElementById('masterclass1').value = 'Masterclass; 1';
+        var label = document.querySelector('label[for=masterclass1]')
+        label.innerHTML = 'Masterclass 1';
+        document.getElementById('masterclass2').value = 'Masterclass 2';
+        label = document.querySelector('label[for=masterclass2]');
+        label.innerHTML = 'Masterclass 2';
+    } else if (waarde == '2') {
+        console.log('sessie 2');
+        document.getElementById('masterclass1').value = 'Masterclass; 3';
+        var label = document.querySelector('label[for=masterclass1]')
+        label.innerHTML = 'Masterclass 3';
+        document.getElementById('masterclass2').value = 'Masterclass 4';
+        label = document.querySelector('label[for=masterclass2]');
+        label.innerHTML = 'Masterclass 4';
+    } else {
+        console.log('sessie 3');
+        document.getElementById('masterclass1').value = 'Masterclass; 5';
+        var label = document.querySelector('label[for=masterclass1]')
+        label.innerHTML = 'Masterclass 5';
+        document.getElementById('masterclass2').value = 'Masterclass 6';
+        label = document.querySelector('label[for=masterclass2]');
+        label.innerHTML = 'Masterclass 6';
+    };
+};
