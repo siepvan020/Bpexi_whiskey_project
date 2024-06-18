cd6c3b8c524da8edb7b0ea134c279a0ba0839f59
BrianVonk <s1105016@student.hsleiden.nl>
2024-06-18 10:33:56 +0200

    Bij meer dan 30 kaarten wordt reservering op reserve lijst geplaatst

--- hielander_whiskey_app/static/js/masterclass_reservering.js
+++ hielander_whiskey_app/static/js/masterclass_reservering.js
@@ -4,26 +4,26 @@ function sessie_selectie() {
     waarde = document.getElementById('sessie_nummer').value;
     if (waarde == '1') {
         console.log('sessie 1');
-        document.getElementById('masterclass1').value = 'Masterclass; 1';
+        document.getElementById('masterclass1').value = 'masterclass; 1';
         var label = document.querySelector('label[for=masterclass1]')
         label.innerHTML = 'Masterclass 1';
-        document.getElementById('masterclass2').value = 'Masterclass 2';
+        document.getElementById('masterclass2').value = 'masterclass 2';
         label = document.querySelector('label[for=masterclass2]');
         label.innerHTML = 'Masterclass 2';
     } else if (waarde == '2') {
         console.log('sessie 2');
-        document.getElementById('masterclass1').value = 'Masterclass; 3';
+        document.getElementById('masterclass1').value = 'masterclass; 3';
         var label = document.querySelector('label[for=masterclass1]')
         label.innerHTML = 'Masterclass 3';
-        document.getElementById('masterclass2').value = 'Masterclass 4';
+        document.getElementById('masterclass2').value = 'masterclass 4';
         label = document.querySelector('label[for=masterclass2]');
         label.innerHTML = 'Masterclass 4';
     } else {
         console.log('sessie 3');
-        document.getElementById('masterclass1').value = 'Masterclass; 5';
+        document.getElementById('masterclass1').value = 'masterclass; 5';
         var label = document.querySelector('label[for=masterclass1]')
         label.innerHTML = 'Masterclass 5';
-        document.getElementById('masterclass2').value = 'Masterclass 6';
+        document.getElementById('masterclass2').value = 'masterclass 6';
         label = document.querySelector('label[for=masterclass2]');
         label.innerHTML = 'Masterclass 6';
     };
