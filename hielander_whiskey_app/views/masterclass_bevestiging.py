3054307f575159e312372f3ac1037e4151a56375
BrianVonk <s1105016@student.hsleiden.nl>
2024-07-01 13:15:11 +0200

    Docs masterclass_bevestiging.py

--- hielander_whiskey_app/views/masterclass_bevestiging.py
+++ hielander_whiskey_app/views/masterclass_bevestiging.py
@@ -7,4 +7,14 @@ from django.shortcuts import render
 
 
 def masterclass_bevestiging_page(request: WSGIRequest) -> HttpResponse:
+    """
+    Deze functie zorgt ervoor dat de pagina
+    'masterclass_bevestiging.html' wordt geladen.
+
+    Args:
+        request: Een HttpRequest-object.
+
+    Returns:
+        Een HttpResponse-object dat de masterclass bevestigings rendert.
+    """
     return render(request, 'masterclass_bevestiging.html')
